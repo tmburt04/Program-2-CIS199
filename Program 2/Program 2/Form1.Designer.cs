c6a244a3551f7f45f20f72b34d53051064c67ba7
tmburt04 <tmburt04@cardmail.louisville.edu>
2017-04-21 11:49:07 -0400

    Initial Creation

---
+++ Program 2/Program 2/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Program_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FreshmanRadioButton = new System.Windows.Forms.RadioButton();
+            this.JuniorRadioButton = new System.Windows.Forms.RadioButton();
+            this.SeniorRadioButton = new System.Windows.Forms.RadioButton();
+            this.SophomoreRadioButton = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // FreshmanRadioButton
+            // 
+            this.FreshmanRadioButton.AutoSize = true;
+            this.FreshmanRadioButton.Location = new System.Drawing.Point(201, 12);
+            this.FreshmanRadioButton.Name = "FreshmanRadioButton";
+            this.FreshmanRadioButton.Size = new System.Drawing.Size(71, 17);
+            this.FreshmanRadioButton.TabIndex = 0;
+            this.FreshmanRadioButton.TabStop = true;
+            this.FreshmanRadioButton.Text = "Freshman";
+            this.FreshmanRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // JuniorRadioButton
+            // 
+            this.JuniorRadioButton.AutoSize = true;
+            this.JuniorRadioButton.Location = new System.Drawing.Point(201, 58);
+            this.JuniorRadioButton.Name = "JuniorRadioButton";
+            this.JuniorRadioButton.Size = new System.Drawing.Size(53, 17);
+            this.JuniorRadioButton.TabIndex = 2;
+            this.JuniorRadioButton.TabStop = true;
+            this.JuniorRadioButton.Text = "Junior";
+            this.JuniorRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // SeniorRadioButton
+            // 
+            this.SeniorRadioButton.AutoSize = true;
+            this.SeniorRadioButton.Location = new System.Drawing.Point(201, 81);
+            this.SeniorRadioButton.Name = "SeniorRadioButton";
+            this.SeniorRadioButton.Size = new System.Drawing.Size(55, 17);
+            this.SeniorRadioButton.TabIndex = 3;
+            this.SeniorRadioButton.TabStop = true;
+            this.SeniorRadioButton.Text = "Senior";
+            this.SeniorRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // SophomoreRadioButton
+            // 
+            this.SophomoreRadioButton.AutoSize = true;
+            this.SophomoreRadioButton.Location = new System.Drawing.Point(201, 35);
+            this.SophomoreRadioButton.Name = "SophomoreRadioButton";
+            this.SophomoreRadioButton.Size = new System.Drawing.Size(79, 17);
+            this.SophomoreRadioButton.TabIndex = 4;
+            this.SophomoreRadioButton.TabStop = true;
+            this.SophomoreRadioButton.Text = "Sophomore";
+            this.SophomoreRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(77, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(93, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Last name:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(52, 58);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Analyze";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 105);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.SophomoreRadioButton);
+            this.Controls.Add(this.SeniorRadioButton);
+            this.Controls.Add(this.JuniorRadioButton);
+            this.Controls.Add(this.FreshmanRadioButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton FreshmanRadioButton;
+        private System.Windows.Forms.RadioButton JuniorRadioButton;
+        private System.Windows.Forms.RadioButton SeniorRadioButton;
+        private System.Windows.Forms.RadioButton SophomoreRadioButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Program 2/Program 2/Form1.cs
@@ -0,0 +1,114 @@
+﻿// Tim Burton
+// Program 2
+// 3-10
+// CIS 199-75-4152
+//
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program_2
+{
+    public partial class Form1 : Form
+    {
+        string day; // actual Registration day
+        string day1 = "Wednesday, April 1st ", day2 = "Thursday, April 2nd ", day3 = "Friday, April 3rd ", day4 = "Monday, April 6th ", day5 = "Tuesday, April 7th ", day6 = "Wednesday, April 8th "; // defines the possible registration days
+        string time; // actual Registration time
+        string time1 = "8:30", time2 = "10:00", time3 = "11:30", time4 = "2:00", time5 = "4:00"; // Possible registration times
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name = textBox1.Text; // gets last name from user
+            char letter = name[0]; // converts last name to first letter of last name
+            if (SeniorRadioButton.Checked || JuniorRadioButton.Checked)
+            {
+                if (SeniorRadioButton.Checked)
+                {
+                    day = day1;
+                }
+                else
+                {
+                    day = day2;
+                }
+                if (letter <= 'a' || letter >= 'd')
+                {
+                    time = time2;
+                }
+                else if (letter <= 'e' || letter >= 'i')
+                {
+                    time = time3;
+                }
+                else if (letter <= 'j' || letter >= 'o')
+                {
+                    time = time4;
+                }
+                else if (letter <= 'p' || letter >= 's')
+                {
+                    time = time5;
+                }
+                else
+                {
+                    time = time1;
+                }
+            }
+
+            if (SophomoreRadioButton.Checked || FreshmanRadioButton.Checked)
+            {
+                if (SophomoreRadioButton.Checked)
+                {
+                    if (letter <= 'f' || letter >= 't')
+                    {
+                        day = day3;
+                    }
+                    else
+                    {
+                        day = day4;
+                    }
+                }
+                else
+                {
+                    if (letter <= 'f' || letter >= 't')
+                    {
+                        day = day5;
+                    }
+                    else
+                    {
+                        day = day6;
+                    }
+                }
+                if (letter <= 'b' || (letter >= 'm' || letter <= 'o'))
+                {
+                    time = time3;
+                }
+                else if ((letter >= 'c' || letter <= 'd') || (letter >= 'p' || letter <= 'q'))
+                {
+                    time = time4;
+                }
+                else if ((letter >= 'e' || letter <= 'f') || (letter >= 'r' || letter <= 's'))
+                {
+                    time = time5;
+                }
+                else if ((letter >= 't' || letter <= 'v') || (letter >= 'g' || letter <= 'i'))
+                {
+                    time = time1;
+                }
+                else if ((letter >= 'w' || letter <= 'z') || (letter >= 'j' || letter <= 'l'))
+                {
+                    time = time2;
+                }
+            }
+            MessageBox.Show("You may register " + day + "at " + time + "."); // Tells user the registration day
+        }
+    }
+}
+
